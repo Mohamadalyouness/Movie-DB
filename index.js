dc08db41896db2fe64b0a55555bd334f98182e16
Mohamadalyouness <mohamadalyouness7@gmail.com>
2023-09-24 23:55:21 +0300

    step 5

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
-
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 //route of the port
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello again')
 })
 
 app.get('/test', (req, res) => {
@@ -43,12 +48,37 @@ app.get('/search', (req, res) => {
     }
 });
 
+app.get('/movies/add', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/movies/get', (req, res) => {
+    res.status(200).json({ status: 200, data: movies });
+});
+
+app.get('/movies/get/by-date', (req, res) => {
+    const sortedMovies = movies.sort((a, b) => {
+      return new Date(a.releaseDate) - new Date(b.releaseDate);
+    });
+    res.status(200).json({ status: 200, data: sortedMovies });
+  });
+  
+
+
+app.get('//movies/edit', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('//movies/delete', (req, res) => {
+    res.send('Hello World')
+})
+
 
 
 
 
 app.listen(3000, () => {
-    console.log('Hello World');
+    console.log('Online');
 })
 
 
